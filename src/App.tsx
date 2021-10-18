19c2d0f7daf94f9f3560c387375ada0a2cb32cef
mkle <mkle@netcompany.com>
2021-10-18 14:23:11 +0200

    FC instead of components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function App() {
-	return (
-		<div>
-			Good luck!
-		</div>
-	);
+const App: React.FC = () => {
+    return (
+        <div>
+            Good luck!
+        </div>
+    );
 }
 
 export default App;
